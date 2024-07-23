3fcd27084befc609b78b71617b726b1934580b36
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-23 16:33:39 +0200

    [upd] seeder for categories table created (bonus 1)

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +14,19 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        //possibilità di svuotare la tabella a ogni esecuzione del seeder
+        //Category::truncate();
+
+        $categories = ['Frontend', 'Backend', 'AI', 'Fullstack'];
+
+        foreach ($categories as $category) {
+
+            $new_category = new Category();
+            $new_category->title = $category;
+            $new_category->slug = Str::of($category)->slug;
+
+            $new_category->save();
+        }
     }
 }
